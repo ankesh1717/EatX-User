71e97ab2ae97d3ff663bac75b84582d2eb8f52c1
ankesh1717 <ankeshpandey123456789@gmail.com>
2025-07-23 14:15:01 +0530

    Updated url

--- src/service/AuthService.js
+++ src/service/AuthService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api";
+const API_URL = "https://foodsapi-1.onrender.com/api";
 
 export const registerUser = async (data) => {
   try {
--- src/service/CartService.js
+++ src/service/CartService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/cart";
+const API_URL = "https://foodsapi-1.onrender.com/api/cart";
 
 const getAuthHeader = (token) => ({
   headers: {
