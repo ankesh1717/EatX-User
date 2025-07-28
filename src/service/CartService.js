d797ea06b393dd62f217e12c8c1837f7ed70e3ac
ankesh1717 <ankeshpandey123456789@gmail.com>
2025-07-29 03:05:00 +0530

    Url updated

--- src/service/CartService.js
+++ src/service/CartService.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "http://localhost:8080/api/cart";
+const API_URL = "https://eatx-api-1.onrender.com/api/cart";
 
 const getAuthHeader = (token) => ({
   headers: {
